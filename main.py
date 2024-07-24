da691ae674b25c873e849c8277f3ce52a021d5a5
Jeff Heller <jeffheller@jhgfd.com>
2024-07-24 15:33:48 -0400

    Reorganized parameters with comments.

--- main.py
+++ main.py
@@ -6,19 +6,28 @@ from Tools.conversion import RomanizationConverter
 def main():
     parser = argparse.ArgumentParser(description='Chinese Romanization Tools')
 
-    parser.add_argument('action', choices=['count_syllables', 'convert'], help='Action to perform')
+    # REQUIRED PARAMETERS
+
     parser.add_argument('text', type=str, help='Text to process')
-    parser.add_argument('--method', type=str, choices=['PY', 'WG'], default='PY',
-                        help='Romanization method (default: PY)')
-    parser.add_argument('--convert', type=str, choices=['PYWG', 'WGPY'], help='Conversion method (PYWG or WGPY)')
+    parser.add_argument('action', choices=['count_syllables', 'convert', 'convert_between'], help='Action to perform')
+
+    # TOOL SELECTION PARAMETERS
+
     parser.add_argument('--skip_count', action='store_true', help='Skip counting syllables')
     parser.add_argument('--method_report', action='store_true', help='Report the romanization method in the output')
+    parser.add_argument('--cherry_pick', type=str, choices=['pinyin', 'wade-giles', 'yale'], help='Convert only identified, valid syllables (enables --error_skip)')
+
+    # CONDITIONAL PARAMETERS
+
+    parser.add_argument('--method', type=str, choices=['pinyin', 'wade-giles', 'yale'], help='Romanization method (default: Pinyin)')
+    parser.add_argument('--from_method', type=str, choices=['pinyin', 'wade-giles', 'yale'], help='Source romanization method (required for convert_between)')
+    parser.add_argument('--to_method', type=str, choices=['pinyin', 'wade-giles', 'yale'], help='Target romanization method (required for convert_between)')
+
+    # OPTIONAL DEBUG PARAMETERS
+
     parser.add_argument('--crumbs', action='store_true', help='Include step-by-step analysis in the output')
-    parser.add_argument('--error_skip', action='store_true', help='Skip errors instead of aborting')
+    parser.add_argument('--error_skip', action='store_true', help='Skip errors instead of aborting (default: True if --cherry_pick is used)')
     parser.add_argument('--error_report', action='store_true', help='Include error messages in the output')
-    parser.add_argument('--cherry_pick', type=str, choices=['PYWG', 'WGPY'],
-                        help='Convert only identified, valid syllables (best for converting paragraphs with multiple '
-                             'languages)')
 
     args = parser.parse_args()
 
