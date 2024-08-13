f85002d2c0aee2dedf9e118d36283f2d4bd1bac3
Jeff Heller <jeffheller@jhgfd.com>
2024-08-13 11:12:29 -0400

    Added 2 tests for crumb reporting variety.

--- tests/test_romanization.py
+++ tests/test_romanization.py
@@ -29,6 +29,40 @@ class TestRomanization(unittest.TestCase):
                            '-----------\n')
         self.assertEqual(mock_stdout.getvalue(), expected_output)
 
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_syllable_count_half_output(self, mock_stdout):
+        syllable_count('ni haa', method='PY', crumbs=True)
+        expected_output = ('# Analyzing ni haa #\n'
+                           '# PY romanization data loaded #\n'
+                           'n [initial]\n'
+                           'n|i [final]\n'
+                           'ni valid: True\n'
+                           'ni syllable count: 1\n'
+                           '-----------\n'
+                           'h [initial]\n'
+                           'h|aa [final]\n'
+                           'haa valid: False\n'
+                           'haa syllable count: 0\n'
+                           '-----------\n')
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_syllable_count_failed_output(self, mock_stdout):
+        syllable_count('noi haa', method='PY', crumbs=True)
+        expected_output = ('# Analyzing noi haa #\n'
+                           '# PY romanization data loaded #\n'
+                           'n [initial]\n'
+                           'n|oi [final]\n'
+                           'noi valid: False\n'
+                           'noi syllable count: 0\n'
+                           '-----------\n'
+                           'h [initial]\n'
+                           'h|aa [final]\n'
+                           'haa valid: False\n'
+                           'haa syllable count: 0\n'
+                           '-----------\n')
+        self.assertEqual(mock_stdout.getvalue(), expected_output)
+
     def test_convert_romanization(self):
         converter = RomanizationConverter('py_wg')  # Initialize only when needed
         result = converter.convert('chang')
