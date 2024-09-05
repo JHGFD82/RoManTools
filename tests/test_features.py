07f3f0d98cc2db9577043d93eb5909825c995d79
Jeff Heller <jeffheller@jhgfd.com>
2024-09-05 10:45:30 -0400

    Removed unused crumb testing (will implement later).

--- tests/test_features.py
+++ tests/test_features.py
@@ -380,58 +380,6 @@ class TestRomanization(unittest.TestCase):
         self.assertEqual(result, [['bai'], ' ', ['ju', 'yi'], ' ', ['li', 'v', 'e', 'd'], ' ', ['du', 'ring'], ' ',
                                   ['the'], ' ', ['mi', 'ddle'], ' ', ['tang'], ' ', ['pe', 'ri', 'o', 'd'], '.'])
 
-    # CRUMBS TESTING #
-    # @patch('sys.stdout', new_callable=StringIO)
-    # def test_syllable_count_output(self, mock_stdout):
-    #     syllable_count('ni hao', method='py', crumbs=True)
-    #     expected_output = ('# Analyzing ni hao #\n'
-    #                        '# PY romanization data loaded #\n'
-    #                        'n [initial]\n'
-    #                        'n|i [final]\n'
-    #                        'ni valid: True\n'
-    #                        'ni syllable count: 1\n'
-    #                        '-----------\n'
-    #                        'h [initial]\n'
-    #                        'h|ao [final]\n'
-    #                        'hao valid: True\n'
-    #                        'hao syllable count: 1\n'
-    #                        '-----------\n')
-    #     self.assertEqual(mock_stdout.getvalue(), expected_output)
-    #
-    # @patch('sys.stdout', new_callable=StringIO)
-    # def test_syllable_count_half_output(self, mock_stdout):
-    #     syllable_count('ni haa', method='py', crumbs=True)
-    #     expected_output = ('# Analyzing ni haa #\n'
-    #                        '# PY romanization data loaded #\n'
-    #                        'n [initial]\n'
-    #                        'n|i [final]\n'
-    #                        'ni valid: True\n'
-    #                        'ni syllable count: 1\n'
-    #                        '-----------\n'
-    #                        'h [initial]\n'
-    #                        'h|aa [final]\n'
-    #                        'haa valid: False\n'
-    #                        'haa syllable count: 0\n'
-    #                        '-----------\n')
-    #     self.assertEqual(mock_stdout.getvalue(), expected_output)
-    #
-    # @patch('sys.stdout', new_callable=StringIO)
-    # def test_syllable_count_failed_output(self, mock_stdout):
-    #     syllable_count('noi haa', method='py', crumbs=True)
-    #     expected_output = ('# Analyzing noi haa #\n'
-    #                        '# PY romanization data loaded #\n'
-    #                        'n [initial]\n'
-    #                        'n|oi [final]\n'
-    #                        'noi valid: False\n'
-    #                        'noi syllable count: 0\n'
-    #                        '-----------\n'
-    #                        'h [initial]\n'
-    #                        'h|aa [final]\n'
-    #                        'haa valid: False\n'
-    #                        'haa syllable count: 0\n'
-    #                        '-----------\n')
-    #     self.assertEqual(mock_stdout.getvalue(), expected_output)
-
 
 if __name__ == '__main__':
     unittest.main()
