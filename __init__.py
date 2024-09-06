b032d413ea5706fead4ab4045ff432a2b4692d75
Jeff Heller <jeffheller@jhgfd.com>
2024-09-06 16:33:19 -0400

    Moved __init__.py back to proper location.

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# __init__.py
+
+from syllable import Syllable
+from conversion import RomanizationConverter
+from data_loader import load_romanization_data, load_stopwords
+from utils import Config, segment_text, validator, convert_text, cherry_pick, syllable_count, detect_method
--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# __init__.py
-
-from .syllable import Syllable
-from .conversion import RomanizationConverter
-from .data_loader import load_romanization_data, load_stopwords
-from .utils import Config, segment_text, validator, convert_text, cherry_pick, syllable_count, detect_method
