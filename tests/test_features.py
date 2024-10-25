2ee89cb92cde0ab528ac86b9cac92ca65df05bd0
Jeff Heller <jeffheller@jhgfd.com>
2024-10-25 11:59:42 -0400

    Adjusted testing for new Wade-Giles input requirements.

--- tests/test_features.py
+++ tests/test_features.py
@@ -149,30 +149,30 @@ class TestRomanization(unittest.TestCase):
 
     @timeit_decorator(repeats=REPEATS)
     def test_segment_text_wade_giles(self):
-        result = segment_text('ni linp’ing shang ch’ankan hsiaoming yüanyang erh shiherh hsiung anwei fenghuang '
-                              'jenmin', method='wg')
-        self.assertEqual(result, [['ni'], ['lin', 'p’ing'], ['shang'], ['ch’an', 'kan'], ['hsiao', 'ming'],
+        result = segment_text("ni lin-p'ing shang ch'an-kan hsiao-ming yüan-yang erh shih-erh hsiung an-wei feng-huang "
+                              "jen-min", method='wg')
+        self.assertEqual(result, [['ni'], ['lin', "p'ing"], ['shang'], ["ch'an", 'kan'], ['hsiao', 'ming'],
                                   ['yüan', 'yang'], ['erh'], ['shih', 'erh'], ['hsiung'], ['an', 'wei'],
                                   ['feng', 'huang'], ['jen', 'min']])
 
     @timeit_decorator(repeats=REPEATS)
     def test_segment_text_ignore_capitalization(self):
-        result = segment_text('Ni linp’ing SHANG ch’ankan Hsiaoming yüanyang erh SHIHERH hsiung Anwei fenghuang '
-                              'jenmin', method='wg')
-        self.assertEqual(result, [['ni'], ['lin', 'p’ing'], ['shang'], ['ch’an', 'kan'], ['hsiao', 'ming'],
+        result = segment_text("Ni lin-p'ing SHANG ch'an-kan Hsiao-ming yüan-yang erh SHIH-ERH hsiung An-wei feng-huang "
+                              "jen-min", method='wg')
+        self.assertEqual(result, [['ni'], ['lin', "p'ing"], ['shang'], ["ch'an", 'kan'], ['hsiao', 'ming'],
                                   ['yüan', 'yang'], ['erh'], ['shih', 'erh'], ['hsiung'], ['an', 'wei'],
                                   ['feng', 'huang'], ['jen', 'min']])
 
     # SYLLABLE VALIDATION TESTING #
     @timeit_decorator(repeats=REPEATS)
     def test_validator_true(self):
-        result = validator('ni linp’ing shang ch’ankan hsiaoming yüanyang erh shiherh hsiung anwei fenghuang',
+        result = validator("ni lin-p'ing shang ch'an-kan hsiao-ming yüan-yang erh shih-erh hsiung an-wei feng-huang",
                            method='wg')
         self.assertEqual(result, True)
 
     @timeit_decorator(repeats=REPEATS)
     def test_validator_false(self):
-        result = validator('ni linp’ing shang ch’anzkan hsiaoming yüanyang erh shiherh hsiung anwei fenghuang',
+        result = validator("ni linp'ing shang ch'anzkan hsiaoming yüanyang erh shiherh hsiung anwei fenghuang",
                            method='wg')
         self.assertEqual(result, False)
 
@@ -198,17 +198,17 @@ class TestRomanization(unittest.TestCase):
     @timeit_decorator(repeats=REPEATS)
     def test_convert_text(self):
         result = convert_text('ni hao chang\'an yuan', convert_from='py', convert_to='wg')
-        self.assertEqual(result, 'ni hao ch’angan yüan')
+        self.assertEqual(result, "ni hao ch'angan yüan")
 
     @timeit_decorator(repeats=REPEATS)
     def test_convert_text_titlecase(self):
         result = convert_text('Ni hao Chang\'an Yuan', convert_from='py', convert_to='wg')
-        self.assertEqual(result, 'Ni hao Ch’angan Yüan')
+        self.assertEqual(result, "Ni hao Ch'angan Yüan")
 
     @timeit_decorator(repeats=REPEATS)
     def test_convert_text_uppercase(self):
         result = convert_text('NI HAO Chang\'an YUAN', convert_from='py', convert_to='wg')
-        self.assertEqual(result, 'NI HAO Ch’angan YÜAN')
+        self.assertEqual(result, "NI HAO Ch'angan YÜAN")
 
     @timeit_decorator(repeats=REPEATS)
     def test_cherry_pick(self):
@@ -220,14 +220,14 @@ class TestRomanization(unittest.TestCase):
                              "career both as a government official and a poet, although these two facets of his career "
                              "seemed to have come in conflict with each other at certain points. Bai Juyi was also a "
                              "devoted Chan Buddhist.", convert_from='py', convert_to='wg')
-        self.assertEqual(result, "Pai Chüi lived during the Middle T’ang period. This was a period of rebuilding and "
-                                 "recovery for the T’ang Empire, following the An Lushan Rebellion, and following the "
+        self.assertEqual(result, "Pai Chüi lived during the Middle T'ang period. This was a period of rebuilding and "
+                                 "recovery for the T'ang Empire, following the An Lushan Rebellion, and following the "
                                  "poetically flourishing era famous for Li Pai (701－762), Wang Wei (701－761), "
                                  "and Tu Fu (712－770). Pai Chüi lived through the reigns of eight or nine emperors, "
-                                 "being born in the Dali regnal era (766-779) of Emperor Taitsung of T’ang. He had a "
+                                 "being born in the Dali regnal era (766-779) of Emperor Taitsung of T'ang. He had a "
                                  "long and successful career both as a government official and a poet, although these "
                                  "two facets of his career seemed to have come in conflict with each other at certain "
-                                 "points. Pai Chüi was also a devoted Ch’an Buddhist.")
+                                 "points. Pai Chüi was also a devoted Ch'an Buddhist.")
 
     @timeit_decorator(repeats=REPEATS)
     def test_cherry_pick_long(self):
@@ -295,28 +295,28 @@ class TestRomanization(unittest.TestCase):
                              "located at Yichang. Bai Juyi was delighted by the flowers and trees for which his new "
                              "location was noted. In 819, he was recalled back to the capital, ending his exile.",
                              convert_from='py', convert_to='wg')
-        self.assertEqual(result, "Pai Chüi lived during the Middle T’ang period. This was a period of rebuilding and "
-                                 "recovery for the T’ang Empire, following the An Lushan Rebellion, and following the "
+        self.assertEqual(result, "Pai Chüi lived during the Middle T'ang period. This was a period of rebuilding and "
+                                 "recovery for the T'ang Empire, following the An Lushan Rebellion, and following the "
                                  "poetically flourishing era famous for Li Pai (701－762), Wang Wei (701－761), "
                                  "and Tu Fu (712－770). Pai Chüi lived through the reigns of eight or nine emperors, "
-                                 "being born in the Dali regnal era (766-779) of Emperor Taitsung of T’ang. He had a "
+                                 "being born in the Dali regnal era (766-779) of Emperor Taitsung of T'ang. He had a "
                                  "long and successful career both as a government official and a poet, although these "
                                  "two facets of his career seemed to have come in conflict with each other at certain "
-                                 "points. Pai Chüi was also a devoted Ch’an Buddhist. Pai Chüi was born in 772 in "
-                                 "T’aiyüan, Shanhsi, which was then a few miles from location of the modern city, "
+                                 "points. Pai Chüi was also a devoted Ch'an Buddhist. Pai Chüi was born in 772 in "
+                                 "T'aiyüan, Shanhsi, which was then a few miles from location of the modern city, "
                                  "although he was in Chengyang, Honan for most of his childhood. His family was poor "
                                  "but scholarly, his father being an Assistant Department Magistrate of the "
                                  "secondclass. At the age of ten he was sent away from his family to avoid a war that "
                                  "broke out in the north of China, and went to live with relatives in the area known "
                                  "as Chiangnan, more specifically Hsüchou. Pai Juyis official career was initially "
                                  "successful. He passed the chinshih examinations in 800. Pai Chüi may have taken up "
-                                 "residence in the western capital city of Ch’angan, in 801. Not long after this, "
+                                 "residence in the western capital city of Ch'angan, in 801. Not long after this, "
                                  "Pai Chüi formed a long friendship with a scholar Yüan Chen. Pai Juyis father died "
                                  "in 804, and the young Pai spent the traditional period of retirement mourning the "
                                  "death of his parent, which he did along the Wei River, near to the capital. 806, "
-                                 "the first full year of the reign of Emperor Hsientsung of T’ang, was the year when "
+                                 "the first full year of the reign of Emperor Hsientsung of T'ang, was the year when "
                                  "Pai Chüi was appointed to a minor post as a government official, at Chouchih, "
-                                 "which was not far from Ch’angan (and also in Shaanxi province). He was made a "
+                                 "which was not far from Ch'angan (and also in Shaanxi province). He was made a "
                                  "member (scholar) of the Hanlin Academy, in 807, and Reminder of the Left from 807 "
                                  "until 815, except when in 811 his mother died, and he spent the traditional "
                                  "threeyear mourning period again along the Wei River, before returning to court in "
@@ -337,12 +337,12 @@ class TestRomanization(unittest.TestCase):
                                  "seeking a pardon, Wu turned to assassination, blaming the Prime Minister, "
                                  "Wu Yüanheng, and other officials: the imperial court generally began by dawn, "
                                  "requiring the ministers to rise early in order to attend in a timely manner; and, "
-                                 "on July 13, 815, before dawn, the T’ang Prime Minister Wu Yüanheng was set to go to "
+                                 "on July 13, 815, before dawn, the T'ang Prime Minister Wu Yüanheng was set to go to "
                                  "the palace for a meeting with Emperor Hsientsung. As he left his house, arrows were "
                                  "fired at his retinue. His servants all fled, and the assassins seized Wu Yüanheng "
                                  "and his horse, and then decapitated him, taking his head with them. The assassins "
                                  "also attacked another official who favored the campaign against the rebellious "
-                                 "warlords, P’ei Tu, but was unable to kill him. The people at the capital were "
+                                 "warlords, P'ei Tu, but was unable to kill him. The people at the capital were "
                                  "shocked and there was turmoil, with officials refusing to leave their personal "
                                  "residences until after dawn. In this context, Pai Chüi overstepped his minor "
                                  "position by memorializing the emperor. As Assistant Secretary to the Princes Tutor, "
@@ -356,9 +356,9 @@ class TestRomanization(unittest.TestCase):
                                  "demoted to the rank of SubPrefect and banished from the court and the capital city "
                                  "to Chiuchiang, then known as Hsün Yang, on the southern shores of the Yangtze River "
                                  "in northwest Chianghsi Province. After three years, he was sent as Governor of a "
-                                 "remote place in Ssuch’uan. At the time, the main travel jout’e there was up the "
+                                 "remote place in Ssuch'uan. At the time, the main travel jout'e there was up the "
                                  "Yangtzu River. This trip allowed Pai Chüi a few days to visit his friend Yüan Chen, "
-                                 "who was also in exile and with whom he explored the rock caves located at Ich’ang. "
+                                 "who was also in exile and with whom he explored the rock caves located at Ich'ang. "
                                  "Pai Chüi was delighted by the flowers and trees for which his new location was "
                                  "noted. In 819, he was recalled back to the capital, ending his exile.")
 
@@ -387,31 +387,31 @@ class TestRomanization(unittest.TestCase):
 
     @timeit_decorator(repeats=REPEATS)
     def test_detect_method_per_word(self):
-        result = detect_method(f"ni linping shang chang'an xiaoming yuanyang er shier xiong anwei fenghuang "
-                               f"renmin shuang yingyong zhongguo qingdao ping'an guangdong hongkong changjiang shen "
+        result = detect_method(f"ni linping shang chang'an xiaoming yuanyang er shier xiong an-wei fenghuang "
+                               f"renmin shuang yingyong zhongguo qingdao p'ing-an guangdong hongkong changjiang shen "
                                f"tingma yia shoiji yiin", per_word=True)
         self.assertEqual(result, [{'word': 'ni', 'methods': ['py', 'wg']},
-                                  {'word': 'linping', 'methods': ['py', 'wg']},
+                                  {'word': 'linping', 'methods': ['py']},
                                   {'word': 'shang', 'methods': ['py', 'wg']},
                                   {'word': "chang'an", 'methods': ['py']},
                                   {'word': 'xiaoming', 'methods': ['py']},
-                                  {'word': 'yuanyang', 'methods': ['py', 'wg']},
+                                  {'word': 'yuanyang', 'methods': ['py']},
                                   {'word': 'er', 'methods': ['py']},
                                   {'word': 'shier', 'methods': ['py']},
                                   {'word': 'xiong', 'methods': ['py']},
-                                  {'word': 'anwei', 'methods': ['py', 'wg']},
-                                  {'word': 'fenghuang', 'methods': ['py', 'wg']},
+                                  {'word': 'an-wei', 'methods': ['py', 'wg']},
+                                  {'word': 'fenghuang', 'methods': ['py']},
                                   {'word': 'renmin', 'methods': ['py']},
                                   {'word': 'shuang', 'methods': ['py', 'wg']},
                                   {'word': 'yingyong', 'methods': ['py']},
                                   {'word': 'zhongguo', 'methods': ['py']},
                                   {'word': 'qingdao', 'methods': ['py']},
-                                  {'word': "ping'an", 'methods': ['py']},
+                                  {'word': "p'ing-an", 'methods': ['wg']},
                                   {'word': 'guangdong', 'methods': ['py']},
                                   {'word': 'hongkong', 'methods': ['py']},
                                   {'word': 'changjiang', 'methods': ['py']},
                                   {'word': 'shen', 'methods': ['py', 'wg']},
-                                  {'word': 'tingma', 'methods': ['py', 'wg']},
+                                  {'word': 'tingma', 'methods': ['py']},
                                   {'word': 'yia', 'methods': []},
                                   {'word': 'shoiji', 'methods': []},
                                   {'word': 'yiin', 'methods': []}]
