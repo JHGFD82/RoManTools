8d54e1774f9305f6d8f62710024c7c70a2745015
Jeff Heller <jeffheller@jhgfd.com>
2024-10-30 13:07:10 -0400

    Adjusted repeats parameter, default now in timeit decorator call.

--- tests/test_features.py
+++ tests/test_features.py
@@ -61,90 +61,89 @@ def generate_random_text_from_list(method, num_syllables=0):
 
 
 class TestRomanization(unittest.TestCase):
-    REPEATS = 100
 
     # PINYIN CHUNK GENERATION TESTING #
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_basic_valid_syllable_vowel(self):
         result = segment_text('a, e, o, ai, ou', method='py')
         self.assertEqual(result, [['a'], ['e'], ['o'], ['ai'], ['ou']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_basic_valid_syllable_initial(self):
         result = segment_text('ba gu zhi ying kai lou yan', method='py')
         self.assertEqual(result, [['ba'], ['gu'], ['zhi'], ['ying'], ['kai'], ['lou'], ['yan']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_complex_finals_er_n_ng(self):
         result = segment_text('han xiang ran ling er', method='py')
         self.assertEqual(result, [['han'], ['xiang'], ['ran'], ['ling'], ['er']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_complex_edge_cases(self):
         result = segment_text('chen chong zhen', method='py')
         self.assertEqual(result, [['chen'], ['chong'], ['zhen']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_multi_syllable_no_apostrophe(self):
         result = segment_text('xiaoming changan wenxin liangxiao', method='py')
         self.assertEqual(result, [['xiao', 'ming'], ['chan', 'gan'], ['wen', 'xin'], ['liang', 'xiao']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_multi_syllable_apostrophe(self):
         result = segment_text("chang'an shan'er li'an", method='py')
         self.assertEqual(result, [['chang', 'an'], ['shan', 'er'], ['li', 'an']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_multi_syllable_vowel_start(self):
         result = segment_text('anwei aiai ouyang ewei', method='py')
         self.assertEqual(result, [['an', 'wei'], ['ai', 'ai'], ['ou', 'yang'], ['e', 'wei']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_invalid_initials(self):
         result = segment_text('xa qo vei', method='py')
         self.assertEqual(result, [['xa'], ['qo'], ['v', 'ei']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_invalid_finals(self):
         result = segment_text('banp zhirr mingk', method='py')
         self.assertEqual(result, [['ban', 'p'], ['zhi', 'rr'], ['ming', 'k']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_special_cases_er_n_ng(self):
         result = segment_text('sheng deng er han shier', method='py')
         self.assertEqual(result, [['sheng'], ['deng'], ['er'], ['han'], ['shi', 'er']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_special_cases_combined_initials(self):
         result = segment_text('shuang huang shun', method='py')
         self.assertEqual(result, [['shuang'], ['huang'], ['shun']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_edge_cases_ambiguous_finals(self):
         result = segment_text('wenti linping gangzhi', method='py')
         self.assertEqual(result, [['wen', 'ti'], ['lin', 'ping'], ['gang', 'zhi']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_no_valid_finals(self):
         result = segment_text('blar ziang shoing', method='py')
         self.assertEqual(result, [['bla', 'r'], ['zi', 'ang'], ['sho', 'ing']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_multi_vowel_combinations(self):
         result = segment_text('ai ei ou uan ie', method='py')
         self.assertEqual(result, [['ai'], ['ei'], ['ou'], ['u', 'an'], ['ie']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_one_syllable_edge_case(self):
         result = segment_text('a e ou yi', method='py')
         self.assertEqual(result, [['a'], ['e'], ['ou'], ['yi']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_multi_syllable_edge_case_er_n_ng(self):
         result = segment_text('zhuangyuan wenxiang gangren', method='py')
         self.assertEqual(result, [['zhuang', 'yuan'], ['wen', 'xiang'], ['gang', 'ren']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_wade_giles(self):
         result = segment_text("ni lin-p'ing shang ch'an-kan hsiao-ming yüan-yang erh shih-erh hsiung an-wei feng-huang "
                               "jen-min", method='wg')
@@ -152,7 +151,7 @@ class TestRomanization(unittest.TestCase):
                                   ['yüan', 'yang'], ['erh'], ['shih', 'erh'], ['hsiung'], ['an', 'wei'],
                                   ['feng', 'huang'], ['jen', 'min']])
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_ignore_capitalization(self):
         result = segment_text("Ni lin-p'ing SHANG ch'an-kan Hsiao-ming yüan-yang erh SHIH-ERH hsiung An-wei feng-huang "
                               "jen-min", method='wg')
@@ -161,19 +160,19 @@ class TestRomanization(unittest.TestCase):
                                   ['feng', 'huang'], ['jen', 'min']])
 
     # SYLLABLE VALIDATION TESTING #
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_validator_true(self):
         result = validator("ni lin-p'ing shang ch'an-kan hsiao-ming yüan-yang erh shih-erh hsiung an-wei feng-huang",
                            method='wg')
         self.assertEqual(result, True)
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_validator_false(self):
         result = validator("ni linp'ing shang ch'anzkan hsiaoming yüanyang erh shiherh hsiung anwei fenghuang",
                            method='wg')
         self.assertEqual(result, False)
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_validator_per_word(self):
         result = validator("ni linpzing shangz chazng'an iaoming yuanng er shizer xiongew anwei fengghuang",
                            method='py', per_word=True)
@@ -192,22 +191,22 @@ class TestRomanization(unittest.TestCase):
                          )
 
     # ROMANIZATON CONVERSION TESTING #
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_convert_text(self):
         result = convert_text('ni hao chang\'an yuan', convert_from='py', convert_to='wg')
-        self.assertEqual(result, "ni hao ch'angan yüan")
+        self.assertEqual(result, "ni hao ch'ang-an yüan")
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_convert_text_titlecase(self):
         result = convert_text('Ni hao Chang\'an Yuan', convert_from='py', convert_to='wg')
-        self.assertEqual(result, "Ni hao Ch'angan Yüan")
+        self.assertEqual(result, "Ni hao Ch'ang-an Yüan")
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_convert_text_uppercase(self):
         result = convert_text('NI HAO Chang\'an YUAN', convert_from='py', convert_to='wg')
-        self.assertEqual(result, "NI HAO Ch'angan YÜAN")
+        self.assertEqual(result, "NI HAO Ch'ang-an YÜAN")
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_cherry_pick(self):
         result = cherry_pick("Bai Juyi lived during the Middle Tang period. This was a period of rebuilding and "
                              "recovery for the Tang Empire, following the An Lushan Rebellion, and following the "
@@ -218,7 +217,7 @@ class TestRomanization(unittest.TestCase):
                              "seemed to have come in conflict with each other at certain points. Bai Juyi was also a "
                              "devoted Chan Buddhist.", convert_from='py', convert_to='wg')
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator(repeats=1)
     def test_cherry_pick_long(self):
         result = cherry_pick("Bai Juyi lived during the Middle Tang period. This was a period of rebuilding and "
                              "recovery for the Tang Empire, following the An Lushan Rebellion, and following the "
@@ -287,7 +286,6 @@ class TestRomanization(unittest.TestCase):
         self.assertEqual(result, "Pai Chü-i lived during the Middle T'ang period. This was a period of rebuilding and recovery for the T'ang Empire, following the An Lu-shan Rebellion, and following the poetically flourishing era famous for Li Pai (701－762), Wang Wei (701－761), and Tu Fu (712－770). Pai Chü-i lived through the reigns of eight or nine emperors, being born in the Ta-li regnal era (766-779) of Emperor Tai-tsung of T'ang. He had a long and successful career both as a government official and a poet, although these two facets of his career seemed to have come in conflict with each other at certain points. Pai Chü-i was also a devoted Ch'an Buddhist. Pai Chü-i was born in 772 in T'ai-yüan, Shan-hsi, which was then a few miles from location of the modern city, although he was in Cheng-yang, Ho-nan for most of his childhood. His family was poor but scholarly, his father being an Assistant Department Magistrate of the second-class. At the age of ten he was sent away from his family to avoid a war that broke out in the north of China, and went to live with relatives in the area known as Chiang-nan, more specifically Hsü-chou. Pai Chü-i's official career was initially successful. He passed the chin-shih examinations in 800. Pai Chü-i may have taken up residence in the western capital city of Ch'ang-an, in 801. Not long after this, Pai Chü-i formed a long friendship with a scholar Yüan Chen. Pai Chü-i's father died in 804, and the young Pai spent the traditional period of retirement mourning the death of his parent, which he did along the Wei River, near to the capital. 806, the first full year of the reign of Emperor Hsien-tsung of T'ang, was the year when Pai Chü-i was appointed to a minor post as a government official, at Chou-chih, which was not far from Ch'ang-an (and also in Shaanxi province). He was made a member (scholar) of the Han-lin Academy, in 807, and Reminder of the Left from 807 until 815, except when in 811 his mother died, and he spent the traditional three-year mourning period again along the Wei River, before returning to court in the winter of 814, where he held the title of Assistant Secretary to the Prince's Tutor. It was not a high-ranking position, but nevertheless one which he was soon to lose. While serving as a minor palace official in 814, Pai managed to get himself in official trouble. He made enemies at court and with certain individuals in other positions. It was partly his written works which led him into trouble. He wrote two long memorials, translated by Arthur Waley as \"On Stopping the War\", regarding what he considered to be an overly lengthy campaign against a minor group of Tatars; and he wrote a series of poems, in which he satirized the actions of greedy officials and highlighting the sufferings of the common folk. At this time, one of the post-An Lu-shan warlords (chieh-tu-shih), Wu Yüan-chi in Ho-nan, had seized control of Chang-i Circuit (centered in Chu-ma-tien), an act for which he sought reconciliation with the imperial government, trying to get an imperial pardon as a necessary prerequisite. Despite the intercession of influential friends, Wu was denied, thus officially putting him in the position of rebellion. Still seeking a pardon, Wu turned to assassination, blaming the Prime Minister, Wu Yüan-heng, and other officials: the imperial court generally began by dawn, requiring the ministers to rise early in order to attend in a timely manner; and, on July 13, 815, before dawn, the T'ang Prime Minister Wu Yüan-heng was set to go to the palace for a meeting with Emperor Hsien-tsung. As he left his house, arrows were fired at his retinue. His servants all fled, and the assassins seized Wu Yüan-heng and his horse, and then decapitated him, taking his head with them. The assassins also attacked another official who favored the campaign against the rebellious warlords, P'ei Tu, but was unable to kill him. The people at the capital were shocked and there was turmoil, with officials refusing to leave their personal residences until after dawn. In this context, Pai Chü-i overstepped his minor position by memorializing the emperor. As Assistant Secretary to the Prince's Tutor, Pai's memorial was a breach of protocol — he should have waited for those of censorial authority to take the lead before offering his own criticism. This was not the only charge which his opponents used against him. His mother had died, apparently caused by falling into a well while looking at some flowers, and two poems written by Pai Chü-i — the titles of which Waley translates as \"In Praise of Flowers\" and \"The New Well\" — were used against him as a sign of lack of Filial Piety, one of the Confucian ideals. The result was exile. Pai Chü-i was demoted to the rank of Sub-Prefect and banished from the court and the capital city to Chiu-chiang, then known as Hsün Yang, on the southern shores of the Yangtze River in northwest Chiang-hsi Province. After three years, he was sent as Governor of a remote place in Ssu-ch'uan. At the time, the main travel route there was up the Yang-tzu River. This trip allowed Pai Chü-i a few days to visit his friend Yüan Chen, who was also in exile and with whom he explored the rock caves located at I-ch'ang. Pai Chü-i was delighted by the flowers and trees for which his new location was noted. In 819, he was recalled back to the capital, ending his exile.")
 
     # SYLLABLE_COUNT TESTING #
-    @timeit_decorator(repeats=REPEATS)
     def test_syllable_count(self):
         result = syllable_count(f"'ni linping shang chang'an xiaoming yuanyang er shier xiong anwei fenghuang "
                                 f"renmin shuang yingyong zhongguo qingdao ping'an guangdong hongkong changjiang shen "
@@ -307,7 +305,7 @@ class TestRomanization(unittest.TestCase):
         result = detect_method(random_text)
         self.assertIn('wg', result, f"Test {repeat_num}: 'wg' not found in text: {random_text}")
 
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_detect_method_per_word(self):
         result = detect_method(f"ni linping shang chang'an xiaoming yuanyang er shier xiong an-wei fenghuang "
                                f"renmin shuang yingyong zhongguo qingdao p'ing-an guangdong hongkong changjiang shen "
@@ -340,7 +338,7 @@ class TestRomanization(unittest.TestCase):
                          )
 
     # ERROR_SKIP TESTING #
-    @timeit_decorator(repeats=REPEATS)
+    @timeit_decorator()
     def test_segment_text_error_skip(self):
         result = segment_text("Bai Juyi lived during the Middle Tang period.", method='py', error_skip=True)
         self.assertEqual(result, [['bai'], ' ', ['ju', 'yi'], ' ', ['li', 'v', 'e', 'd'], ' ', ['du', 'ring'], ' ',
