1735c8b7be0718be08bb3b2b588ff94e499df518
Jeff Heller <jeffheller@jhgfd.com>
2024-11-15 15:50:49 -0500

    Imported new Word object into utils.

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
-from typing import Dict, Union, List, Tuple, Set
+from typing import Dict, Union, List, Tuple
 from .config import Config
 from .chunk import TextChunkProcessor
 from .syllable import Syllable
+from .word import WordProcessor
 from .conversion import RomanizationConverter
 from .data_loader import load_method_params, load_stopwords
 from functools import lru_cache
