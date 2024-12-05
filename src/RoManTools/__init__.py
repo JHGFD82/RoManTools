db7ef831d22914ed0c57b0fddb0eb254581cd4d6
Jeff Heller <jeffheller@jhgfd.com>
2024-12-05 10:14:38 -0500

    Added docstring.

--- src/RoManTools/__init__.py
+++ src/RoManTools/__init__.py
@@ -1,4 +1,32 @@
-# __init__.py
+"""
+RoManTools
+
+This package provides tools for processing romanized Mandarin text, including segmentation, validation, conversion, and
+syllable counting. It currently supports romanization methods Pinyin and Wade-Giles.
+
+Modules:
+    utils: Utility functions for text segmentation, validation, conversion, and syllable counting.
+
+Functions:
+    segment_text: Segments text into words and non-text segments.
+    validator: Validates the romanized text.
+    convert_text: Converts text between different romanization methods.
+    cherry_pick: Selectively processes text based on specific criteria.
+    syllable_count: Counts the number of syllables in the text.
+    detect_method: Detects the romanization method used in the text.
+
+Usage Example:
+    >>> from RoManTools import convert_text
+    >>> text = "Zhongguo ti'an tianqi"
+    >>> converted_text = convert_text(text, from_method='py', to_method='wg')
+    >>> print(converted_text)
+
+Author:
+    Jeff Heller <jsheller@princeton.edu>
+
+Version:
+    1.0.0
+"""
 
 from .utils import segment_text, validator, convert_text, cherry_pick, syllable_count, detect_method
 
