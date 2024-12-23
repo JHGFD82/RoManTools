2a70bad829316c63c1d7e02ab96e47df3f5f5082
Jeff Heller <jeffheller@jhgfd.com>
2024-12-23 17:39:22 -0500

    Added exception as variable for debugging.

--- RoManTools/data_loader.py
+++ RoManTools/data_loader.py
@@ -84,8 +84,8 @@ def load_method_params(method: str) -> Dict[str, Union[List[str], np.ndarray]]:
     method_file = f'{method}DF'
     try:
         init_list, fin_list, ar = load_romanization_data(os.path.join(base_path, 'data', f'{method_file}.csv'))
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Syllable array for method '{method}' not found.")
+    except FileNotFoundError as exc:
+        raise FileNotFoundError(f"Syllable array for method '{method}' not found.") from exc
     # if config.crumbs:
     #     print(f"# {method.upper()} romanization data loaded #")
     return {
